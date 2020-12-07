070bd22637ca9a37b9c4ebcdee3f67a7bca35c30
Saurabh Chauhan <saurabh311@gmail.com>
2020-12-07 13:19:37 +0100

    date picker for assinment post

--- frontend/src/components/assignmentpost/AssignmentPostForm.jsx
+++ frontend/src/components/assignmentpost/AssignmentPostForm.jsx
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from "react";
 import { format } from 'date-fns';
 import Auth from '../../services/Auth';
 import UserApi from '../../api/UserApi';
-
+import DatePicker from "../calendar/DatePicker";
 import FileUploader from '../announcementNew/FileUploader';
 
 //last date submission
@@ -97,8 +97,18 @@ return (
               <option value='English'>English</option>
               <option value='Science'>Science</option>
               <option value='GK'>General Knowledge</option>
-             
+              
             </select>
+            
+          </div>
+          <div class='form-group '>
+          <label for='inputType'>Last Submission Date</label>
+        
+              
+            
+              <DatePicker />
+              
+            
           </div>
             
           <button type="submit" className="btn-newPost"> Submit </button>
