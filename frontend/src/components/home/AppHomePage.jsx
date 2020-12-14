f93ef6e8eed617eafa4418b91f272652567eb9b0
snehalkdm2191 <snehalkdm2191@gmail.com>
2020-12-14 13:22:29 +0100

    as per student grade enabled disabled grad card.

--- frontend/src/components/home/AppHomePage.jsx
+++ frontend/src/components/home/AppHomePage.jsx
@@ -1,140 +1,204 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import Auth from '../../services/Auth';
-import UserApi from '../../api/UserApi';
+import Auth from "../../services/Auth";
+import UserApi from "../../api/UserApi";
 
-
-import AssignmentPost from "../assignmentpost/AssignmentPostPage";
-import grade1 from '../../images/abc.png';
-import grade2 from '../../images/read.png';
-import grade3 from '../../images/classroom.png';
-import grade4 from '../../images/human.png';
-import gradeAs1 from '../../images/online-learning.png';
-import gradeAs2 from '../../images/student-3.png';
-import gradeAs3 from '../../images/student-2.png';
-import gradeAs4 from '../../images/reading.png';
-
+import grade1 from "../../images/abc.png";
+import grade2 from "../../images/read.png";
+import grade3 from "../../images/classroom.png";
+import grade4 from "../../images/human.png";
+import gradeAs1 from "../../images/online-learning.png";
+import gradeAs2 from "../../images/student-3.png";
+import gradeAs3 from "../../images/student-2.png";
+import gradeAs4 from "../../images/reading.png";
 
 function AppHomePage() {
-    const [gradeone,setGradeone]=useState("1");
-    const [gradetwo,setGradetwo]=useState("2");
-    const [gradethree,setGradethree]=useState("3");
-    const [gradefour,setGradefour]=useState("4");
-    const [grade,setGrade]=useState('1');
-    const [user, setUser] = useState({});
-    const userMail = Auth.getUserMail();
+  const [grade, setGrade] = useState("1");
+  const [user, setUser] = useState({});
+  const userMail = Auth.getUserMail();
 
-    
-    useEffect(() => {
-        function getUserByMail() {
-            UserApi.getUserByMail(userMail)
-                .then((res) => {
-                    setUser(res.data)
-                })
-        }
-        userMail !== null && getUserByMail();
-    }, [userMail])
-    
+  useEffect(() => {
+    function getUserByMail() {
+      UserApi.getUserByMail(userMail).then((res) => {
+        setUser(res.data);
+      });
+    }
+    userMail !== null && getUserByMail();
+  }, [userMail]);
+
   return (
-      <div>
-        <span className="Assignement-title">Study Materials</span><br/><br/>
-        <div class="row">
+    <div>
+      <span className="Assignement-title">Study Materials {user.grade}</span>
+      <br />
+      <br />
+      <div class="row">
         <div class="col-sm-5">
-        <div class="card grade grade-1">
+          <div class="card grade grade-1">
             <div class="card-body">
-            <h5 class="card-title grade-title"> 
-            <Link className=""  to={{ pathname: `/studymaterial/grade/${grade}`, state: {grade:1} }} >
-                    <img className="grade-img" src={gradeAs1} alt="" srcset=""/>&nbsp;Grade - 1
-            </Link>
-          
-            </h5>
+              <Link
+                className={user.grade === '1' ? "" : "isDisabled"}
+                to= {user.grade === '1' ? 
+                {
+                  pathname: `/studymaterial/grade/${grade}`,
+                  state: { grade: 1 },
+                } : ""}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={grade1} alt="" srcset="" />
+                  &nbsp;Grade - 1
+                </h5>
+              </Link>
             </div>
-        </div>
-        </div>
-        <div class="col-sm-5">
-        <div class="card grade grade-2">
-            <div class="card-body">
-            <Link className=""  to={{ pathname: `/studymaterial/grade/${grade}`, state: {grade:2} }} >
-            <h5 class="card-title grade-title"><img className="grade-img" src={gradeAs2} alt="" srcset=""/>&nbsp;Grade - 2</h5>
-            </Link>
-            </div>
-        </div>
-        </div>
-        <div class="col-sm-5">
-        <div class="card grade grade-3">
-            <div class="card-body">
-            <Link className=""  to={{ pathname: `/studymaterial/grade/${grade}`, state: {grade:3} }} >
-            <h5 class="card-title grade-title"><img className="grade-img" src={gradeAs3} alt="" srcset=""/>&nbsp;Grade - 3</h5>
-            </Link>
-            </div>
-        </div>
+          </div>
         </div>
         <div class="col-sm-5">
-        <div class="card grade grade-4">
+          <div class="card grade grade-2">
             <div class="card-body">
-            <Link className=""  to={{ pathname: `/studymaterial/grade/${grade}`, state: {grade:4} }} >
-            <h5 class="card-title grade-title"><img className="grade-img" src={gradeAs4} alt="" srcset=""/>&nbsp;Grade - 4</h5>
-            </Link>
+              <Link
+                className={user.grade === '2' ? "" : "isDisabled"}
+                to= {user.grade === '2' ? 
+                {
+                  pathname: `/studymaterial/grade/${grade}`,
+                  state: { grade: 2 },
+                } : ""}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={grade2} alt="" srcset="" />
+                  &nbsp;Grade - 2
+                </h5>
+              </Link>
             </div>
+          </div>
         </div>
+        <div class="col-sm-5">
+          <div class="card grade grade-3">
+            <div class="card-body">
+              <Link
+                className={user.grade === '3' ? "" : "isDisabled"}
+                to= {user.grade === '3' ? 
+                {
+                  pathname: `/studymaterial/grade/${grade}`,
+                  state: { grade: 3 },
+                }: '' }
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={grade3} alt="" srcset="" />
+                  &nbsp;Grade - 3
+                </h5>
+              </Link>
+            </div>
+          </div>
         </div>
+        <div class="col-sm-5">
+          <div class="card grade grade-4">
+            <div class="card-body">
+              <Link
+                className={user.grade === '4' ? "" : "isDisabled"}
+                to= {user.grade === '4' ? 
+                {
+                  pathname: `/studymaterial/grade/${grade}`,
+                  state: { grade: 4 },
+                } : ''}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={grade4} alt="" srcset="" />
+                  &nbsp;Grade - 4
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <br />
+
+      <span className="Assignement-title">Assignments</span>
+      <br />
+      <br />
+      <div class="row">
+        <div class="col-sm-5">
+          <div class="card grade gradeAs-1">
+            <div class="card-body">
+              <Link
+                className={user.grade === '1' ? "" : "isDisabled"}
+                to= {user.grade === '1' ? 
+                {
+                  pathname: `/assignmentPost/grade/${grade}`,
+                  state: { grade: 1 },
+                } : ''}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={gradeAs1} alt="" srcset="" />
+                  &nbsp;Grade - 1
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="card grade gradeAs-2">
+            <div class="card-body">
+              <Link
+                className={user.grade === '2' ? "" : "isDisabled"}
+                to= {user.grade === '2' ? 
+                {
+                  pathname: `/assignmentPost/grade/${grade}`,
+                  state: { grade: 2 },
+                } : ''}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={gradeAs2} alt="" srcset="" />
+                  &nbsp;Grade - 2
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="card grade gradeAs-3">
+            <div class="card-body">
+              <Link
+                className={user.grade === '3' ? "" : "isDisabled"}
+                to= {user.grade === '3' ? 
+                {
+                  pathname: `/assignmentPost/grade/${grade}`,
+                  state: { grade: 3 },
+                } : ''}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={gradeAs3} alt="" srcset="" />
+                  &nbsp;Grade - 3
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="card grade gradeAs-4">
+            <div class="card-body">
+              <Link
+                className={user.grade === '4' ? "" : "isDisabled"}
+                to= {user.grade === '4' ? 
+                {
+                  pathname: `/assignmentPost/grade/${grade}`,
+                  state: { grade: 4 },
+                } : ''}
+              >
+                <h5 class="card-title grade-title">
+                  <img className="grade-img" src={gradeAs4} alt="" srcset="" />
+                  &nbsp;Grade - 4
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
     </div>
-
-    <br/><br/>
-
-    <span className="Assignement-title">Assignments</span><br/><br/>
-        <div class="row">
-        <div class="col-sm-5">
-        <div class="card grade gradeAs-1">
-            <div class="card-body">
-            <h5 class="card-title grade-title">
-            
-                <Link className=""  to={{ pathname: `/assignmentPost/grade/${grade}`, state: {grade:1} }} >
-                    <img className="grade-img" src={gradeAs1} alt="" srcset=""/>&nbsp;Grade - 1
-                </Link>
-            </h5>
-            </div>
-        </div>
-        </div>
-        <div class="col-sm-5">
-        <div class="card grade gradeAs-2">
-            <div class="card-body">
-            <h5 class="card-title grade-title">
-
-            <Link className="" to={{ pathname: `/assignmentPost/grade/${grade}`, state: {grade:2} }} >
-                <img className="grade-img" src={gradeAs2} alt="" srcset=""/>&nbsp;Grade - 2
-            </Link>  
-            </h5>
-            </div>
-        </div>
-        </div>
-        <div class="col-sm-5">
-        <div class="card grade gradeAs-3">
-            <div class="card-body">
-            <h5 class="card-title grade-title">
-            <Link className="" to={{ pathname: `/assignmentPost/grade/${grade}`, state: {grade:3} }} >
-                <img className="grade-img" src={gradeAs3} alt="" srcset=""/>&nbsp;Grade - 3
-            </Link>
-            </h5>
-            </div>
-        </div>
-        </div>
-        <div class="col-sm-5">
-        <div class="card grade gradeAs-4">
-            <div class="card-body">
-            <h5 class="card-title grade-title">
-            <Link className="" to={{ pathname: `/assignmentPost/grade/${grade}`, state: {grade:4} }} >
-                <img className="grade-img" src={gradeAs4} alt="" srcset=""/>&nbsp;Grade - 4
-            </Link>
-            </h5>
-            </div>
-        </div>
-        </div>
-    </div>
-    <br/><br/>
-    </div> 
-  )
+  );
 }
 
 export default AppHomePage;
