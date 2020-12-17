4c385d7f4304b739458bfc7b5a8c1962f04b9bb1
snehalkdm2191 <snehalkdm2191@gmail.com>
2020-12-17 17:01:55 +0100

    changes done for announcement card issue.

--- frontend/src/components/announcementNew/AnnouncementCard.jsx
+++ frontend/src/components/announcementNew/AnnouncementCard.jsx
@@ -50,68 +50,68 @@ function AnnouncementCard({ announcement, user }) {
   };
 
   return (
-    <div className="col-md-6 announcement-post">
-      <section className="widget">
-        <div className="widget-body">
-          <div className="widget-top-overflow text-white">
+    <div class="col-md-6 announcement-post">
+      <section class="widget">
+        <div class="widget-body">
+          <div class="widget-top-overflow text-white">
             {url &&
               (url.match(".gif") ||
               url.match(".jpg") ||
               url.match(".png") ||
               url.match(".jpeg") ? (
-                <image
+                <img
                   src={announce.imageUrl}
-                  className="img-fluid"
+                  class="img-fluid"
                   alt="Responsive image"
                 />
               ) : (
-                <div className="embed-responsive embed-responsive-16by9">
+                <div class="embed-responsive embed-responsive-16by9">
                   <iframe title="announcement1"
-                    className="embed-responsive-item"
+                    class="embed-responsive-item"
                     src={announce.imageUrl}
                     allowfullscreen
                   ></iframe>
                 </div>
               ))}
           </div>
-          <div className="post-user mt-sm">
-            <span className="thumb pull-left mr">
-              <img className="img-circle" src={announce.user.imgUrl} alt="..." />
+          <div class="post-user mt-sm">
+            <span class="thumb pull-left mr">
+              <img class="img-circle" src={announce.user.imgUrl} alt="..." />
             </span>
-            <h5 className="poster-detail">
-              <span className="fw-semi-bold">{announce.user.name}</span>{" "}
+            <h5 class="poster-detail">
+              <span class="fw-semi-bold">{announce.user.name}</span>{" "}
             </h5>
             <h6>
-              <span className="fw-semi-bold">{announce.user.email}</span>{" "}
+              <span class="fw-semi-bold">{announce.user.email}</span>{" "}
             </h6>
           </div>
           <br />
-          <h5 className="mb-xs mt-xs">
-            <span className="fw-semi-bold">{announce.title}</span>{" "}
+          <h5 class="mb-xs mt-xs">
+            <span class="fw-semi-bold">{announce.title}</span>{" "}
           </h5>
-          <p className="fs-mini m txt-color">{announce.body} </p>
+          <p class="fs-mini m txt-color">{announce.body} </p>
         </div>
-        <footer className="bg-body-light">
-          <ul className="post-links no-separator">
-            <li classNameName="cmt-like">
+        <footer class="bg-body-light">
+          <ul class="post-links no-separator">
+            <li className="cmt-like">
               <button onClick={updateLike}>
-                <i className="fa fa-heart"></i> {announce.likes}
+                <i class="fa fa-heart"></i> {announce.likes}
               </button>
             </li>
 
             {user.email === announce.user.email ? (
-              <button classNameName="btn btn-light mr-s" onClick={deletePost}>
-                <i className="fas fa-trash"></i>
+              <button className="btn btn-light mr-s" onClick={deletePost}>
+                <i class="fas fa-trash"></i>
               </button>
             ) : null}
-            <li classNameName="cmt-like">
+            <li className="cmt-like">
               <a href="#">
-                <i className="fa fa-comment"></i> {userComment.length}{" "}
+                <i class="fa fa-comment"></i> {userComment.length}{" "}
               </a>
             </li>
-            <li classNameName="btn-view">
+            <li className="btn-view">
               <Link
-                classNameName="btn btn-outline-info"
+                className="btn btn-outline-info"
                 to={{
                   pathname: `/announce/${announce.id}`,
                   state: { announce },
